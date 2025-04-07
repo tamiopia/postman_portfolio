f7e6dac7dc218ea1a6f2ae523920319d2f0402f0
tamiopia <tamiopia23@gmail.com>
2025-04-07 12:57:18 +0300

    analytincs added

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { PostmanInterface } from "../components/postman-interface"
+import { Analytics } from "@vercel/analytics/react"
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-[#1c1c1c]">
       <PostmanInterface />
+      <Analytics />
     </main>
   )
 }
